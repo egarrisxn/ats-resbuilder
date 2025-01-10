4dd5193ee230af0463a431e701444383ac84610c
egarrisxn <egarrisxn@gmail.com>
2025-01-10 14:14:34 -0500

    udpated dates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
 export default function Footer() {
+  const date = new Date().getFullYear()
   return (
     <footer className='flex h-10 flex-row items-center justify-between border-t-2 border-slate-400 bg-white px-2 sm:px-4 md:px-24'>
       <div className='flex flex-row gap-2 text-sm sm:gap-4 sm:text-base md:gap-8'>
@@ -13,7 +14,7 @@ export default function Footer() {
           Source Code
         </a>
       </div>
-      <div className='flex text-sm font-bold sm:text-base'>EG|2024</div>
+      <div className='flex text-sm font-bold sm:text-base'>EG|{date}</div>
     </footer>
   )
 }
